aa8eb30c2b0d8cbd317b84f294ee61854823fdcd
Ethan Voth <87922019+evoth@users.noreply.github.com>
2023-01-15 20:06:46 -0500

    API Functionality + Docs (#2)
    * Added basic contacts api and docs
    
    * Fixed README
    
    * Tweaked API parameters, switched database users
    
    * Specify "userId" or "contactId" instead of "id"
    
    * (Tested again with Postman)

---
+++ html/LAMPAPI/UpdateContact.php
@@ -0,0 +1,44 @@
+<?php
+	$inData = getRequestInfo();
+
+	$conn = new mysqli("localhost", "Carl", "APIGUY", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("UPDATE Contacts SET FirstName=?, LastName=?, "
+            . "Phone=?, Email=? WHERE ID=?;");
+		$stmt->bind_param(
+            "ssssi",
+            $inData["firstName"],
+            $inData["lastName"], 
+            $inData["phone"],
+            $inData["email"],
+            $inData["contactId"]
+        );
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
